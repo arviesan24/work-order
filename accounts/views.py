06d82219fb443e29ac1f0064321b55832fe3ab98
Romel Santoral <ctechnology24@gmail.com>
2019-06-16 23:52:37 +0800

    Set `accounts` views

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+"""Views for accounts app."""
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+
+from allauth.account.views import LoginView
+
+
+class CustomUserLogin(LoginView):
+    """Custom View for user login."""
+
+    template_name = 'accounts/login.html'
+    success_url = reverse_lazy('home')
+
+
+class HomeTemplateView(LoginRequiredMixin, TemplateView):
+    """TemplateView for home page."""
+
+    template_name = 'home.html'
