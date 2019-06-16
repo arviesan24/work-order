8184ad652047494e588044a4da33b41712782b3d
Romel Santoral <ctechnology24@gmail.com>
2019-06-16 22:43:48 +0800

    Set `AUTHENTICATION_BACKENDS`

--- workorder/settings.py
+++ workorder/settings.py
@@ -52,6 +52,16 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'workers.Worker'
 
 
+# 
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
