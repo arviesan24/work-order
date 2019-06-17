4dde3117e142543d2e8f9e8e8eed7076cb249396
Romel Santoral <ctechnology24@gmail.com>
2019-06-17 22:04:40 +0800

    Create `WorkOrder` model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
+"""Models for orders app."""
+
 from django.db import models
 
-# Create your models here.
+
+class WorkOrder(models.Model):
+    """Model for WorkOrder."""
+
+    workers = models.ForeignKey(
+        'workers.Worker', on_delete=models.SET_NULL, null=True,
+        related_name='workorders', related_query_name='workorder')
+    title = models.CharField(max_length=250, blank=False)
+    description = models.TextField(blank=False)
+    deadline = models.DateField(blank=False)
