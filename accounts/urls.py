8af21743e9974db9ccb77e23896dde18c05376a8
Romel Santoral <ctechnology24@gmail.com>
2019-06-17 20:41:30 +0800

    Create `CustomUserLogoutView` url

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.CustomUserLoginView.as_view(), name='user-login'),
+    path('logout/', views.CustomUserLogoutView.as_view(), name='user-logout'),
     path('signup/', views.CustomUserSignupView.as_view(), name='user-signup'),
 ]
