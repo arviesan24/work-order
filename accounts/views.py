8507b2701e186023d3f36405dcf69c946813afa9
Romel Santoral <ctechnology24@gmail.com>
2019-06-17 18:29:45 +0800

    Import `login`

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 """Views for accounts app."""
 
 from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
