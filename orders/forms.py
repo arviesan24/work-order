29ee792bf67d46e3cc59d05c91b061ad40b13247
Romel Santoral <ctechnology24@gmail.com>
2019-06-18 16:13:57 +0800

    Create `orders.forms`

---
+++ orders/forms.py
@@ -0,0 +1,21 @@
+"""Forms for orders app."""
+
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from . import models
+
+
+class WorkOrderForm(forms.ModelForm):
+    """Form for WOrkOrder model."""
+
+    def clean(self):
+        """Return validated input."""
+        super().clean()
+        workers = self.cleaned_data.get('workers', [])
+
+        # Raise error if user enters more than 5 workers.
+        if len(workers) > 5:
+            raise ValidationError(
+                _('Only 5 workers are allowed per WorkOrder.'))
