35a95999ebe5b13434b64879a508c5f511dd8879
Romel Santoral <ctechnology24@gmail.com>
2019-06-18 16:17:22 +0800

    Import `receiver`

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 
 class WorkOrder(models.Model):
