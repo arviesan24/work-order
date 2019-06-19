8bbfdf559f8f68538380202fb1827cfd0256e676
Romel Santoral <ctechnology24@gmail.com>
2019-06-19 16:34:32 +0800

    Create `OrderViewSet`

--- orders/viewsets.py
+++ orders/viewsets.py
@@ -3,3 +3,10 @@
 from rest_framework import viewsets
 
 from . import serializers
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """Viewset for OrderSerializer."""
+
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
