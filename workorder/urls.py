63ac208231d479c1c9bfd10fa8db8872478ceb0e
Romel Santoral <ctechnology24@gmail.com>
2019-06-19 16:56:03 +0800

    Include `orders.urls_api` in project urls

--- workorder/urls.py
+++ workorder/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
         include(('workers.urls', 'workers'), namespace='workers')),
 
     # rest framework URLs
+    path('', include('orders.urls_api')),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
 ]
