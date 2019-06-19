f2d748791f1fd2bfae9ef189df912bae38cf923e
Romel Santoral <ctechnology24@gmail.com>
2019-06-19 16:29:00 +0800

    Create `OrderSerializer`

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,3 +3,11 @@
 from rest_framework import serializers
 
 from . import models
+
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer for Order model."""
+
+    class Meta:
+        model = models.Order
+        fields = ['url', 'id', 'workers', 'title', 'description', 'deadline']
