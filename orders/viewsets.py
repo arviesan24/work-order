d62361ef9901e8f3c486c4aa7d31dee78bb075a2
Romel Santoral <ctechnology24@gmail.com>
2019-06-21 19:06:13 +0800

    Set `WorkOrderViewSet` `filterset_class`

--- orders/viewsets.py
+++ orders/viewsets.py
@@ -21,3 +21,4 @@ class WorkOrderViewSet(viewsets.ModelViewSet):
     queryset = models.WorkOrder.objects.all()
     serializer_class = serializers.WorkOrderSerializer
     filter_backends = (django_filters.DjangoFilterBackend,)
+    filterset_class = WorkOrderFilterSet
