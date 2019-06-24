5e357739798d74b651a379d674cb921faa9db39c
Romel Santoral <ctechnology24@gmail.com>
2019-06-24 21:54:23 +0800

    Set `WorkOrderSerializer` `depth` to 1

--- orders/serializers.py
+++ orders/serializers.py
@@ -13,6 +13,7 @@ class WorkOrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.WorkOrder
         fields = ['url', 'id', 'workers', 'title', 'description', 'deadline']
+        depth = 1
 
     def validate_workers(self, value):
         """Return validated `workers` value."""
