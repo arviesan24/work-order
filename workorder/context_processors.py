6cc7bebb8294a0b5b252efeaf3ae4da5557f22c4
Romel Santoral <ctechnology24@gmail.com>
2019-06-24 16:21:36 +0800

    Create `custom_data` in context_processors

--- workorder/context_processors.py
+++ workorder/context_processors.py
@@ -1,3 +1,12 @@
 """Project custom context processors."""
 
 from django.contrib.sites.models import Site
+
+
+def custom_data(context):
+    """Return addional context to templates."""
+    current_site = Site.objects.get_current()
+    
+    return {
+        'CURRENT_SITE': current_site.domain,
+        'API_ROOT_URL': f'{current_site.domain}/api/'}
