9e9562f57acde42cc0b01ddff022bb25a77567fc
Romel Santoral <ctechnology24@gmail.com>
2019-06-24 16:52:26 +0800

    load dotenv

--- workorder/settings.py
+++ workorder/settings.py
@@ -14,6 +14,9 @@ import os
 import json
 
 from django.urls import reverse_lazy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
