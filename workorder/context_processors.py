18b078694e3d86bd628d3d39931c9a193d147e17
Romel Santoral <ctechnology24@gmail.com>
2019-06-24 19:43:06 +0800

    Add `DEBUG` in context processors

--- workorder/context_processors.py
+++ workorder/context_processors.py
@@ -9,5 +9,6 @@ def custom_data(context):
     current_site = Site.objects.get_current()
     
     return {
+        'DEBUG': settings.DEBUG,
         'CURRENT_SITE': current_site.domain,
         'API_ROOT_URL': f'{current_site.domain}/api/'}
