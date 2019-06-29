9dac41011252e87d0fc26c50313c82c40cab0a21
Romel Santoral <ctechnology24@gmail.com>
2019-06-29 22:58:25 +0800

    Create `WorkerTemplateView`

--- workers/views.py
+++ workers/views.py
@@ -2,3 +2,9 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
+
+
+class WorkerTemplateView(LoginRequiredMixin, TemplateView):
+    """TemplateView for Worker objects."""
+
+    template_name = 'workers/list.html'
