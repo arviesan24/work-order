3ffe9543f25806f52d0bdc045b63533b1cbe0070
Romel Santoral <ctechnology24@gmail.com>
2019-06-30 18:01:50 +0800

    Set `STATIC_ROOT`

--- workorder/settings.py
+++ workorder/settings.py
@@ -187,3 +187,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.getenv(
+    'STATIC_ROOT',
+    os.path.join(os.path.dirname(BASE_DIR), 'public', 'static'))
