9a8f3cbc8bae8bf07fec88679466b6a8b0bd9c13
Romel Santoral <ctechnology24@gmail.com>
2019-06-30 17:49:54 +0800

    Add `Allow HTTPS` in settings

--- workorder/settings.py
+++ workorder/settings.py
@@ -91,6 +91,19 @@ REST_FRAMEWORK = {
 }
 
 
+# Allow HTTPS on the server
+if not DEBUG:
+    CORS_REPLACE_HTTPS_REFERER      = True
+    HOST_SCHEME                     = "https://"
+    SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT             = True
+    SESSION_COOKIE_SECURE           = True
+    CSRF_COOKIE_SECURE              = True
+    SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+    SECURE_HSTS_SECONDS             = 1000000
+    SECURE_FRAME_DENY               = True
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
