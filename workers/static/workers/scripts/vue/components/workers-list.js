c83d99eca3f1ca0e0c21b2af7bc88ef0ed1fdb52
Romel Santoral <ctechnology24@gmail.com>
2019-07-05 20:34:09 +0800

    Wrap workers list to `table-responsive` div

--- workers/static/workers/scripts/vue/components/workers-list.js
+++ workers/static/workers/scripts/vue/components/workers-list.js
@@ -9,25 +9,27 @@ Vue.component('workers-list', {
     }
   },
   template: `
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th scope="col">Delete</th>
-        <th scope="col">Name</th>
-        <th scope="col">Company</th>
-        <th scope="col">Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="worker in workers">
-        <th scope="row">
-          <button v-on:click="emitDeleteWorker(worker)" class="btn btn-danger btn-sm mr-2">x</button>
-        </th>
-        <td>[[ worker.name ]]</td>
-        <td>[[ worker.company ]]</td>
-        <td>[[ worker.email ]]</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-responsive">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Delete</th>
+          <th scope="col">Name</th>
+          <th scope="col">Company</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="worker in workers">
+          <th scope="row">
+            <button v-on:click="emitDeleteWorker(worker)" class="btn btn-danger btn-sm mr-2">x</button>
+          </th>
+          <td>[[ worker.name ]]</td>
+          <td>[[ worker.company ]]</td>
+          <td>[[ worker.email ]]</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   `
 })
